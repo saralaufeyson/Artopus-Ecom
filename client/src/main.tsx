8b7d508f05275bb451c18d02579b47cb5db8d8cc
Sara Laufeyson <layasree81103@gmail.com>
2026-06-04 21:29:21 +0530

    trying to connect backend

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import axios from 'axios'
 import './index.css'
 import App from './App.tsx'
+
+const apiUrl = import.meta.env.VITE_API_URL?.replace(/\/+$/, '')
+
+if (apiUrl) {
+  axios.defaults.baseURL = apiUrl
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
